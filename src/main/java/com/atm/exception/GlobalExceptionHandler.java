0bc8020e32ac48487a34157c6d129af7802a2165
RadhaChavan <radhachavan8459@gmail.com>
2025-09-06 01:03:01 +0530

    create Exception to handle Global Exceptions

---
+++ src/main/java/com/atm/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.atm.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleAuthException(AuthenticationException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+}
