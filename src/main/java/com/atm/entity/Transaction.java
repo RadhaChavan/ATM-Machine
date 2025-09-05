e4f5f643527dc0da3b331ead2de5e9a05ff851fb
RadhaChavan <radhachavan8459@gmail.com>
2025-09-06 00:39:21 +0530

    add Transaction entity class

---
+++ src/main/java/com/atm/entity/Transaction.java
@@ -0,0 +1,65 @@
+package com.atm.entity;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Transaction {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+    private BigDecimal amount;
+    private Timestamp timestamp;
+    private String details;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Account getAccount() {
+		return account;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	public TransactionType getType() {
+		return type;
+	}
+	public void setType(TransactionType type) {
+		this.type = type;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+    
+}
